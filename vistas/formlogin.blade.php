ed76ba0865f922f6835eb2cc97379540322b1243
Ivan Rodriguez <jivanrod@gmail.com>
2018-03-16 11:26:47 +0100

    Fix validation server

--- vistas/formlogin.blade.php
+++ vistas/formlogin.blade.php
@@ -22,8 +22,8 @@ Formulario login
                     <div class="form-group row">                            
                         <label for="inputNombre" class="col-sm-2 col-form-label">Nombre</label>
                         <div class="col-sm-10">
-                            <input id="inputNombre" type="text" value="{{ (isset($nombre) ) ? $nombre : "" }}"
-                                   class="{{ (isset($nombre) && !$nombre) ? "form-control is-invalid col-sm-10" : "form-control col-sm-10" }}" 
+                            <input id="inputNombre" type="text" value="{{ (isset($nombre) && $nombre) ? $nombre : "" }}"
+                                   class="form-control  col-sm-10 {{ (isset($nombre) && !$nombre) ? "is-invalid" : "is-valid" }}" 
                                    id="inputNombre" placeholder="Nombre" name="nombre">
                             <div class="col-sm-10 invalid-feedback">
                                 El nombre es obligatorio y tiene entre 3 y 25 caracteres
@@ -34,8 +34,9 @@ Formulario login
                     <div class="form-group row">
                         <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
                         <div class="col-sm-10">
-                            <input type="password" value="{{ (isset($clave) ) ? $clave : "" }}"
-                                   class="{{ (isset($clave) && !$clave) ? "form-control is-invalid col-sm-10" : "form-control col-sm-10" }}" id="inputPassword" placeholder="Password" name="clave">
+                            <input type="password" value="{{ (isset($clave) && $clave) ? $clave : "" }}"
+                                   class="form-control  col-sm-10 {{ (isset($clave) && !$clave) ? "is-invalid" : "is-valid" }}" 
+                                   id="inputPassword" placeholder="Password" name="clave">
                             <div class="col-sm-10 invalid-feedback">
                                 La clave tiene entre 4 y 8 caracteres e incluye al menos un número
                             </div>
@@ -55,25 +56,3 @@ Formulario login
 </div>
 @endsection
 
-@section ('script')
-<script>
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-    (function () {
-        'use strict';
-        window.addEventListener('load', function () {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.getElementsByClassName('formlogin');
-            // Loop over them and prevent submission
-            var validation = Array.prototype.filter.call(forms, function (form) {
-                form.addEventListener('submit', function (event) {
-                    if (form.checkValidity() === false) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-                    form.classList.add('was-validated');
-                }, false);
-            });
-        }, false);
-    })();
-</script>
-@endsection
