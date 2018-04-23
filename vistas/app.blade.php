cc13f9f151b856052779a18acc5c4f497d4591b1
Ivan Rodriguez <jivanrod@gmail.com>
2018-04-23 16:22:23 +0200

    Fix value in modeVal

--- vistas/app.blade.php
+++ vistas/app.blade.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
         crossorigin="anonymous">
     <script>
-        var modoVal='{{ $modoVal }}'
+        var modoVal='{{ isset($modoVal) ? $modoVal : ""}}'
     </script>
 </head>
 
